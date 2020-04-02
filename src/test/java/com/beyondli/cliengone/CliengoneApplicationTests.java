905c0cc48acc7300dd72fe70ec0e77d4aaf356eb
dlz <dlz0729@126.com>
2020-04-02 17:33:34 +0800

    clientone

---
+++ src/test/java/com/beyondli/cliengone/CliengoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyondli.cliengone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliengoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

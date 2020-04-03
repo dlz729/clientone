1db874b5a90b11e766c2b30e2941206ae954f197
dlz <dlz0729@126.com>
2020-04-03 08:58:03 +0800

    clientone

--- src/main/java/com/beyondli/clientone/ClientoneApplication.java
+++ src/main/java/com/beyondli/clientone/ClientoneApplication.java
@@ -1,4 +1,4 @@
-package com.beyondli.cliengone;
+package com.beyondli.clientone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CliengoneApplication {
+//客户端启动类
+public class ClientoneApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CliengoneApplication.class, args);
+        SpringApplication.run(ClientoneApplication.class, args);
     }
 
 }
